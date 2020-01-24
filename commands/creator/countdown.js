51dcbb39bd4e147ee8eb138f02d1c25f0ad089ed
yumi <caubedondoc.ls01@gmail.com>
2020-01-24 22:18:05 +0700

    jasdjaskdja

--- commands/creator/countdown.js
+++ commands/creator/countdown.js
@@ -4,23 +4,18 @@ const data = {
     caller: "countdown",
     cd: 10,
     run: function(msg=new Discord.Message,params=[]){
-        console.log(params[1],params[2],params[3]);
-        return;
+        // console.log(params[1],params[2],params[3]);
+        // return;
         var guild = msg.member.guild;
+        if (!guild) return;
         var channedl = guild.channels.get(params[1])
-        guilds.forEach(guild=>{
-            var channel;
-            if (!guildExtend.welcomeChannel) return
-            var em = new Discord.RichEmbed();
-            em.setTitle("Notice from the creator!")
-            em.setDescription(args)
-            if(!channel){
-                channel= guild.channels.find(c=>c.type=="text"&&c.permissionsFor(guild.me).has("SEND_MESSAGES"))
-                em.addBlankField();
-                em.addField("For Guild Master","If you're Admin,you should create a `bot-logs` or `bot-spam` channel for Yumi's notice :3 Thanks!")
-            }
-            channel.send(em);
-        })
+        if (!channedl) return;
+        if (!params[2].match(new RegExp('/^\d{1,}$/'))) return;
+        params.splice(0,3);
+        var content = params.join(' ');
+        setTimeout(()=>{
+            msg.channel.send(content);
+        },parseInt(params[2]));
     }
 }
 module.exports = new Command(data)
