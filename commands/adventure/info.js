afe76f222a4e20e1d00b6014212c59d1176d28b7
yumi <thanhduyls97@gmail.com>
2020-09-28 01:35:59 +0700

    Add command meminfo

---
+++ commands/adventure/info.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const Command = require('../../models/Command');
+const Bot = require('../../models/Bot');
+const Adventure = require('../../models/Adventure');
+const data = {
+    caller: "meminfo",
+    cd: 10,
+    description: "Check member info",
+    nsfw: false,
+    help: [],
+    enable: true,
+    alias: [],
+    run: async function (bot = new Bot({}), msg = new Discord.Message, params = []) {
+        const sexs = ["shounen ♂ ", "shoujo ♀ ", "futanari ⚤ "];
+        const races = await bot.execsql("select * from adv_race");
+        const dummy_field = { name: '\u200B', value: '\u200B' };
+        const dummy_field_inline = { name: '\u200B', value: '\u200B', inline: true };
+        if (bot.members.has(msg.author.id)) {
+            let member = bot.members.get(msg.author.id);
+            let em = new Discord.MessageEmbed;
+            em.setTitle("[Member Info]")
+                .setThumbnail(msg.author.displayAvatarURL({size: 256}))
+                .addFields([
+                    {
+                        name: "Name",
+                        value: member.name
+                    },
+                ])
+                .addFields([
+                    {
+                        name: "Birth",
+                        value: "00/00/00",
+                        inline: true
+                    },
+                    {
+                        name: "Sex",
+                        value: sexs[member.sex - 1],
+                        inline: true
+                    },
+                    {
+                        name: "Race",
+                        value: (races.find(m => m.race_id == member.race)).race_name,
+                        inline: true
+                    }
+                ])
+                .addFields([
+                    {
+                        name: "Level",
+                        value: member.level,
+                        inline: true
+                    },
+                    {
+                        name: "EXP",
+                        value: `${member.exp} / ${member.exp_max} [${(member.exp * 100 / member.exp_max).toFixed(2)} %]`,
+                        inline: true
+                    }, dummy_field_inline
+                ]);
+            msg.channel.send(em);
+        }
+    }
+}
+module.exports = new Command(data)
